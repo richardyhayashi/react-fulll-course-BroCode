5cb1a4933d497c5e3e473e40dcd455f308051fd7
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-03-16 12:04:26 +0900

    Finished useEffect hook

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,9 @@
-import MyComponent from "./components/MyComponent";
+import MyComponent2 from "./components/MyComponent2";
 
 function App() {
   return (
     <>
-      <MyComponent />
+      <MyComponent2 />
     </>
   );
 }
